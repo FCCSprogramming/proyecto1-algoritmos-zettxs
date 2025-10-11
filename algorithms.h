71e44ace8455d8c364c4164837cce3da4d9bd216
zett <enriquesilvadeyra@gmail.com>
2025-10-10 23:39:22 -0500

    Actualización de algorithms.h

---
+++ algorithms.h
@@ -0,0 +1,18 @@
+#ifndef ALGORITHMS_H
+#define ALGORITHMS_H
+
+//funciones auxiliares
+void intercambio (int &a, int &b);
+void imprimir (int arr[], int n);
+//algoritmos de ordenamiento
+void burbuja (int arr[], int n);
+void seleccion (int arr[], int n);
+void insersion (int arr[], int n);
+//algoritmos de búsqueda
+void merge (int arr[], int inicio, int medio, int fin);
+void mergesort (int arr[], int inicio, int fin);
+
+
+
+
+#endif 
